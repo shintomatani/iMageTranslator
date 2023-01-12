fb333af4ea5522d9cc5a0506350bf1f79461592c
shintomatani <est812@icloud.com>
2023-01-12 18:25:53 +0900

    fixed

--- Flask/static/js/main.js
+++ Flask/static/js/main.js
@@ -2,9 +2,9 @@ $(function() {
   $('#edit').click(function(){
     $('#orig_txt').slideToggle();
   });
-});
 
-$('#upload .fileinput').on('change', function () {
- var file = $(this).prop('files')[0];
- $(this).closest('#upload').find('#filename').text(file.name);
+  $('#upload .fileinput').on('change', function () {
+    var file = $(this).prop('files')[0];
+    $(this).closest('#upload').find('#filename').text(file.name);
+  });
 });
